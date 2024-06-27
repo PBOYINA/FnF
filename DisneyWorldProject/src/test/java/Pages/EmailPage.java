ca284c46ddc77d4f5b84c143c82913a4d7bfe104
raji <rajimol.s@annulartechnologies.com>
2024-06-27 23:02:39 +0530

    Deleted old code

--- DisneyWorldProject/src/test/java/Pages/EmailPage.java
+++
@@ -1,9 +0,0 @@
-package Pages;
-
-import org.openqa.selenium.By;
-
-public class EmailPage {
-	public static By emailreceived = By.xpath("//td[contains(text(),\"You have a new Family & Friends invite!\")]");
-	public static By mailinatorSearch = By.id("inbox_field");
-	public static By go = By.className("primary-btn");
-}
--- DisneyWorldProject/src/test/java/config/Config.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-public class Config {
-	// Disney
-	public static String disney = "https://disneyworld.disney.go.com/";
-	//public static String disney = "https://www.disneyworld.eu/";
-	public static String mailinator = "https://www.mailinator.com/v4/public/inboxes.jsp";
-}
